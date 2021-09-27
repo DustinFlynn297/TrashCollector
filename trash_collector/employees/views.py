6dcbabaa055b3fbf55fbaf871c9529364f56b843
NiGeanya Hawkins <nmhawkins@outlook.com>
2021-09-27 15:56:57 -0400

    created index function for views: employees can now register for an account after logging in.

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
 from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from datetime import date
+
 
 from .models import Employee
 
@@ -9,12 +13,27 @@ from .models import Employee
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
+    logged_in_user = request.user
+    try:
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today,
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))   
+
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
 
+@login_required
 def create(request):
     logged_in_user = request.user
     if request.method == "POST":
